76dc16276a3978a319e969a977c02c9818117d96
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-05 14:45:02 -0600

    set up for index done

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+
+server.get('/', (req, res, next) => {{
+    res.send(`<h1>Welcome to my simple api</h1>`)
+}})
+
+server.use('*', (req, res, next) => {
+    res.json({
+        message: 'This is my simple api'
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`server running PORT: ${PORT}`)
+})
